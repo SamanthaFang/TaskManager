9786e00378957c14f2485eb33fdc392f32392949
SamanthaFang <13045249@myrp.edu.sg>
2017-05-26 15:06:41 +0800

    Initial commit

---
+++ app/src/main/java/a13045249/c347/soi/rp/edu/sg/taskmanager/Task.java
@@ -0,0 +1,18 @@
+package a13045249.c347.soi.rp.edu.sg.taskmanager;
+
+import java.io.Serializable;
+
+/**
+ * Created by 13045249 on 26/5/2017.
+ */
+
+public class Task implements Serializable{
+    private ;
+    private ;
+
+    public Task(){
+        this. =;
+        this. =;
+    }
+
+}
